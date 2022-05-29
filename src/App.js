4f2711aa47847fd8ea601ee9af5df5faf4a678ed
KimMinchan95 <hialscks@gmail.com>
2022-05-29 15:18:01 +0900

    practice recoil

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { atom, RecoilRoot, selector, useRecoilState, useRecoilValue } from 'recoil';
 
-function App() {
+const usernameState = atom({
+  key: 'username',
+  default: 'Red'
+})
+
+const countState = selector({
+  key: 'count',
+  get: ({ get }) => {
+    const username = get(usernameState);
+    return username.length;
+  }
+})
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <RecoilRoot>
+      <Nav />
+      <Body />
+    </RecoilRoot>
+  )
+}
+
+const Nav = () => {
+  const username = useRecoilValue(usernameState);
+
+  return (
+    <div className="nav">
+      <p>{username}</p>
     </div>
-  );
+  )
+}
+
+const Body = () => {
+  return (
+    <div className="body">
+      <Profile />
+      <Count />
+    </div>
+  )
+}
+
+const Profile = () => {
+  const [username, setUserName] = useRecoilState(usernameState);
+  return (
+    <div>
+      <h2>Profile:</h2>
+      <p>{ username }</p>
+      <input type='text' value={username} onChange={e => setUserName(e.target.value)} />
+    </div>
+  )
+}
+
+const Count = () => {
+  const count = useRecoilValue(countState);
+  return (
+    <div>
+      <p>Count: {count}</p>
+    </div>
+  )
 }
 
 export default App;
